590df5ffc7e03a030c11d1ec52b5ec736512653d
TheIndieStation <s.dieters.gdd@gmail.com>
2020-11-10 15:52:19 +0100

    Update InfinityCube.ino

--- InfinityCube/InfinityCube.ino
+++ InfinityCube/InfinityCube.ino
@@ -7,8 +7,9 @@
 CRGB leds[NumPixels];
 CHSV kleur = CHSV(135, 255, 255);
 
-int corners[8][6] = {
-    {70, -69, 55, -54, -14, 15}
+int corners[8][6][2] = {
+    {70, -69, 55, -54, -14, 15},
+    {-24, 25, 80, -99, -114, 115}
   };
 
 void setup()
@@ -22,30 +23,55 @@ void setup()
 
 void loop() 
 {
-    fromCorner(0, 100);
-    delay(1000);
+    fromCorner(0, 100, false);
+    delay(0);
+    //fill_solid(leds, NumPixels, CRGB(0,0,0));
+    //FastLED.show();
+    delay(0);
+    toCorner(1, 100, true);
+    delay(0);
     fill_solid(leds, NumPixels, CRGB(0,0,0));
-    FastLED.show();
-    delay(250);
+    //FastLED.show();
+    delay(0);
+    fromCorner(1, 100, false);
+    delay(0);
+    //fill_solid(leds, NumPixels, CRGB(0,0,0));
+    //FastLED.show();
+    delay(0);
+    toCorner(0, 100, true);
+    delay(0);
+    fill_solid(leds, NumPixels, CRGB(0,0,0));
+    //FastLED.show();
+    delay(0);
 }
 
-void fromCorner(int corner, int wait){
+void fromCorner(int corner, int wait, bool invert){
   for(int i = 0; i < 5; i++){
     for(int j = 0; j < 6; j++){
         int multiplier = abs(corners[corner][j]) / corners[corner][j];
-        Serial.println(abs(corners[corner][j]) + (i * multiplier));
-        leds[abs(corners[corner][j]) + (i * multiplier)] = CHSV(135, 255, 100 - ((100/5) * i));
+        int value;
+        if(invert){
+            value = (i + 1) * 20;
+          } else {
+              value = 100 - ((i - 1) * 20);
+            }
+        leds[abs(corners[corner][j]) + (i * multiplier)] = CHSV(135, 255, value);
       }
       FastLED.show();
       delay(wait);
     }
   }
-void toCorner(int corner, int wait){
-  for(int i = 4; i > 0; i--){
+void toCorner(int corner, int wait, bool invert){
+  for(int i = 4; i >= 0; i--){
     for(int j = 0; j < 6; j++){
         int multiplier = abs(corners[corner][j]) / corners[corner][j];
-        Serial.println(abs(corners[corner][j]) + (i * multiplier));
-        leds[abs(corners[corner][j]) + (i * multiplier)] = CHSV(135, 255, 100 - ((100/5) * i));
+        int value;
+        if(invert){
+            value = (i + 1) * 20;
+          } else {
+              value = 100 - ((i - 1) * 20);
+            }
+        leds[abs(corners[corner][j]) + (i * multiplier)] = CHSV(135, 255, value);
       }
       FastLED.show();
       delay(wait);
